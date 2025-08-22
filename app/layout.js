0e7f234d6075015c494e95bbb4daec39d1a3cd59
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-08-22 19:03:34 +0530

    Build the home page

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ const josefin = Josefin_Sans({
 })
 
 console.log(josefin);
-import "@app/_styles/globals.css"
+import "@/app/_styles/globals.css"
 import Header from "./_components/Header";
 export const metadata = {
   // title: "The Wild Oasis",
@@ -23,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} antialiased  bg-colors-primary-950 text-colors-primary-100 min-h-screen flex flex-col `} >
+      <body className={`${josefin.className} antialiased  bg-colors-primary-950 text-colors-primary-100 min-h-screen flex flex-col relative `} >
         <Header />
         <div className="flex-1 px-8 py-12" >  <main className="max-w-7xl mx-auto" >{children} </main></div>
         <footer> Copyright by The Wild Oasis </footer>
