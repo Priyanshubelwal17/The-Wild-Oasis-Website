d927dcb336c486b075b44c86392f8c72010fd1f3
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-08-30 16:40:13 +0530

    Implement the context API for client-side state management

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,6 +1,7 @@
 "use client"
 
 import { isWithinInterval } from "date-fns";
+
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 
@@ -15,12 +16,13 @@ function isAlreadyBooked(range, datesArr) {
 }
 
 function DateSelector({ settings, bookedDates, cabin }) {
+
+
   // CHANGE
   const regularPrice = 23;
   const discount = 23;
   const numNights = 23;
   const cabinPrice = 23;
-  const range = { from: null, to: null };
 
   // SETTINGS
   const { minBookingLength, maxBookingLength } = settings
@@ -29,6 +31,8 @@ function DateSelector({ settings, bookedDates, cabin }) {
       <DayPicker
         className="pt-12 place-self-center"
         mode="range"
+        onSelect={(range) => setRange(range)}
+        selected={range}
         min={minBookingLength + 1}
         max={maxBookingLength}
         fromMonth={new Date()}
