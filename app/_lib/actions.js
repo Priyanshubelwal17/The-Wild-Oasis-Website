99bef5a03fc93290e8fee11a495fddbc9fe10690
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-09-03 19:04:37 +0530

    Create and connect updateGuest server action to profile form

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,10 @@
 import { redirect } from "next/dist/server/api-utils";
 import { signIn, signOut } from "./auth";
 
+export async function updateGuest() {
+    console.log("Server action");
+}
+
 export async function signInAction() {
     await signIn("google", { redirectTo: "/account" })
 }
