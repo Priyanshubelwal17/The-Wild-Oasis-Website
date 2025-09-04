ed5d6c91a3c8f95b3842864b94a46d8f1348365c
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-09-04 11:28:15 +0530

    Add revalidation and authorization to delete action

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { redirect } from "next/dist/server/api-utils";
 import { auth, signIn, signOut } from "./auth";
 import supabase from "./supabase";
 import { revalidatePath } from "next/cache";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
     const session = await auth();
@@ -39,12 +40,16 @@ export async function deleteReservation(bookingId) {
     const session = await auth();
     if (!session) throw new Error("You must be logged in")
 
+    const guestBookings = await getBookings(session.user.guestId)
+    const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
     const { error } = await supabase.from('bookings').delete().eq('id', bookingId);
 
     if (error)
 
         throw new Error('Booking could not be deleted');
 
+    revalidatePath('/account/reservations')
 }
 
 export async function signInAction() {
