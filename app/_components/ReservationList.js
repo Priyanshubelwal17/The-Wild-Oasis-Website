0f25a482dc83b93190f0a8cf3c0d7e6253c365dc
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-09-08 20:01:17 +0530

    Fix a bug rename deletereservation to deleteBooking

--- app/_components/ReservationList.js
+++ app/_components/ReservationList.js
@@ -2,14 +2,13 @@
 
 import { useOptimistic } from "react";
 import ReservationCard from "./ReservationCard";
-import DeleteReservation from '@/app/_components/DeleteReservation';
 import { deleteBooking } from "../_lib/actions";
 
 function ReservationList({ bookings }) {
     const [optimisticBookings, optimisticDelete] = useOptimistic(bookings, (curBookings, bookingId) => { return curBookings.filter((booking) => booking.id !== bookingId) })
     async function handleDelete(bookingId) {
         optimisticDelete(bookingId)
-        await deleteReservation(bookingId)
+        await deleteBooking(bookingId)
     }
     return (
         <div>
